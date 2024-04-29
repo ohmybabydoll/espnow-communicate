9be8b110c88686389580b33124ed14c70ec28e6c
ohmybabydoll <31585274+ohmybabydoll@users.noreply.github.com>
2024-04-29 21:59:17 +0800

    add queue to write i2s

--- main/espnow.c
+++ main/espnow.c
@@ -24,17 +24,17 @@
 #define QUEUE_RECV_DELAY 100
 #define MY_CODE 1234
 #define GPIO_INPUT_PIN GPIO_NUM_0
-#define ESPNOW_DATA_SIZE 128
+#define ESPNOW_DATA_SIZE 248
 
 static uint8_t s_example_broadcast_mac[ESP_NOW_ETH_ALEN] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 static uint16_t s_example_espnow_seq[EXAMPLE_ESPNOW_DATA_MAX] = {0, 0};
 static const char *TAG = "espnow";
 
 static void example_espnow_deinit(esp_now_data_packet *data_packet);
-static uint16_t *recive_16bit;                   // 接收后转换的16位数据，长度128
 static uint16_t *output_16bit;                   // 发送给扬声器的16位数据，长度256，因为传入数据是双声道所以*2
 static esp_now_data_packet *send_broadcast_data; // 用于发送广播配对
 static bool send_over = true;
+static QueueHandle_t audio_queue;
 
 static void wifi_init(void)
 {
@@ -46,6 +46,16 @@ static void wifi_init(void)
     ESP_ERROR_CHECK(esp_wifi_set_mode(ESPNOW_WIFI_MODE));
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_ERROR_CHECK(esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
+}
+
+static void queue_init(void)
+{
+    audio_queue = xQueueCreate(30, sizeof(esp_now_data_packet));
+    if (audio_queue == NULL)
+    {
+        ESP_LOGE(TAG, "Queue creation failed");
+        exit(1);
+    }
 }
 
 /* ESPNOW sending or receiving callback function is called in WiFi task.
@@ -108,18 +118,18 @@ static void example_espnow_recv_cb(const esp_now_recv_info_t *recv_info, const u
     else if (MY_CODE == recv_data->code)
     {
 
-        int payload_len = len - sizeof(esp_now_data_packet);
-        int len_16bit = ESPNOW_DATA_SIZE / 2;
-        // 将recv_data->data转为16bit数据
-        for (int i = 0; i < len_16bit; i++)
+        // int payload_len = len - sizeof(esp_now_data_packet);
+        // int len_16bit = ESPNOW_DATA_SIZE / 2;
+        // // 将recv_data->data转为16bit数据
+        // for (int i = 0; i < len_16bit; i++)
+        // {
+        //     output_16bit[i] = (recv_data->data[i * 2] << 8) | recv_data->data[i * 2 + 1];
+        // }
+        if (xQueueSend(audio_queue, recv_data, QUEUE_RECV_DELAY) != pdTRUE)
         {
-            output_16bit[i] = (recv_data->data[i * 2] << 8) | recv_data->data[i * 2 + 1];
+            ESP_LOGE(TAG, "Queue send failed");
         }
-
-        if (!i2s_write_max98357(output_16bit, len_16bit))
-        {
-            ESP_LOGE(TAG, "i2s write failed");
-        }
+        // i2s_write_max98357(output_16bit, len_16bit);
     }
     else
     {
@@ -215,6 +225,7 @@ static void example_espnow_deinit(esp_now_data_packet *data_packet)
 {
     free(data_packet->data);
     free(data_packet);
+    vSemaphoreDelete(audio_queue);
     esp_now_deinit();
 }
 
@@ -270,57 +281,79 @@ void espnow_create_send_i2s_task(void)
     xTaskCreate(read_data_task, "read_data_task", 1024, NULL, 4, NULL);
 }
 
-void test_send_sine_wave(void)
+void write_audio_queue(void *args)
 {
-    int buffer_size = ESPNOW_DATA_SIZE / 2;
-    uint16_t buffer[buffer_size];
-    generate_sine_wave(buffer, buffer_size, 16000);
+    esp_now_data_packet recv_data;
+    int len_16bit = ESPNOW_DATA_SIZE / 2;
 
-    esp_now_peer_info_t peer_uni;
-    esp_now_fetch_peer(true, &peer_uni);
-
-    int send_len = ESPNOW_DATA_SIZE + sizeof(esp_now_data_packet);
-    uint8_t *r_buf = (uint8_t *)calloc(1, ESPNOW_DATA_SIZE);
-    assert(r_buf); // Check if s_buf allocation success
-
-    for (uint8_t i = 0; i < buffer_size; i++)
-    {
-        r_buf[i * 2] = (uint8_t)(buffer[i] >> 8);
-        r_buf[i * 2 + 1] = (uint8_t)(buffer[i] & 0xff);
-    }
-
-    esp_now_data_packet *send_data;
-    send_data = malloc(send_len);
-    if (send_data == NULL)
-    {
-        ESP_LOGE(TAG, "Malloc send data fail");
-        esp_now_deinit();
-        return;
-    }
-    send_data->code = MY_CODE;
-    memcpy(send_data->data, r_buf, ESPNOW_DATA_SIZE);
-    int count = 782;
     while (1)
     {
-        if (gpio_get_level(GPIO_INPUT_PIN) != 0)
+        if (xQueueReceive(audio_queue, &recv_data, portMAX_DELAY) == pdTRUE)
         {
-            vTaskDelay(pdMS_TO_TICKS(10));
-            continue;
-        }
-        while (!send_over)
-        {
-            vTaskDelay(pdMS_TO_TICKS(1));
-        }
-        count--;
-        send_over = false;
-        esp_err_t err_t = esp_now_send(peer_uni.peer_addr, send_data, send_len);
-        if (err_t != ESP_OK)
-        {
-            ESP_LOGE(TAG, "Send error to " MACSTR ", err_t: %d", MAC2STR(peer_uni.peer_addr), err_t);
+            for (int i = 0; i < len_16bit; i++)
+            {
+                output_16bit[i] = (recv_data.data[i * 2] << 8) | recv_data.data[i * 2 + 1];
+            }
+            i2s_write_max98357(output_16bit, ESPNOW_DATA_SIZE / 2);
+        } else {
+            vTaskDelay(pdMS_TO_TICKS(20));
         }
     }
-    free(send_data);
 }
+
+// void test_send_sine_wave(void)
+// {
+
+//     ESP_LOGI(TAG, "test send sine wave ");
+//     int buffer_size = ESPNOW_DATA_SIZE / 2;
+//     uint16_t buffer[buffer_size];
+//     generate_sine_wave(buffer, buffer_size, 16000);
+
+//     esp_now_peer_info_t peer_uni;
+//     esp_now_fetch_peer(true, &peer_uni);
+
+//     int send_len = ESPNOW_DATA_SIZE + sizeof(esp_now_data_packet);
+//     uint8_t *r_buf = (uint8_t *)calloc(1, ESPNOW_DATA_SIZE);
+//     assert(r_buf); // Check if s_buf allocation success
+
+//     for (uint8_t i = 0; i < buffer_size; i++)
+//     {
+//         r_buf[i * 2] = (uint8_t)(buffer[i] >> 8);
+//         r_buf[i * 2 + 1] = (uint8_t)(buffer[i] & 0xff);
+//     }
+
+//     esp_now_data_packet *send_data;
+//     send_data = malloc(send_len);
+//     if (send_data == NULL)
+//     {
+//         ESP_LOGE(TAG, "Malloc send data fail");
+//         esp_now_deinit();
+//         return;
+//     }
+//     send_data->code = MY_CODE;
+//     memcpy(send_data->data, r_buf, ESPNOW_DATA_SIZE);
+
+//     while (1)
+//     {
+//         if (gpio_get_level(GPIO_INPUT_PIN) != 0)
+//         {
+//             vTaskDelay(pdMS_TO_TICKS(10));
+//             continue;
+//         }
+//         while (!send_over)
+//         {
+//             vTaskDelay(pdMS_TO_TICKS(1));
+//         }
+
+//         send_over = false;
+//         esp_err_t err_t = esp_now_send(peer_uni.peer_addr, send_data, send_len);
+//         if (err_t != ESP_OK)
+//         {
+//             ESP_LOGE(TAG, "Send error to " MACSTR ", err_t: %d", MAC2STR(peer_uni.peer_addr), err_t);
+//         }
+//     }
+//     free(send_data);
+// }
 
 void espnow_global_init(void)
 {
@@ -336,11 +369,13 @@ void espnow_global_init(void)
     ESP_ERROR_CHECK(espnow_init());
     ESP_ERROR_CHECK(init_broadcast_send_data());
     ESP_ERROR_CHECK(auto_peer());
-    recive_16bit = (uint16_t *)malloc(sizeof(uint16_t) * ESPNOW_DATA_SIZE);
     output_16bit = (uint16_t *)malloc(sizeof(uint16_t) * ESPNOW_DATA_SIZE / 2);
+    queue_init();
+
+    xTaskCreate(write_audio_queue, "write_audio_queue", 1024, NULL, 4, NULL);
 
     // Check if memory allocation was successful
-    if (recive_16bit == NULL || output_16bit == NULL)
+    if (output_16bit == NULL)
     {
         ESP_LOGE(TAG, "Memory allocation failed\n");
         exit(1);
--- main/main.c
+++ main/main.c
@@ -10,7 +10,6 @@
 #include "esp_random.h"
 #include "esp_mac.h"
 
-
 #define GPIO_INPUT_PIN GPIO_NUM_0
 
 static const char *READ_I2S_TAG = "READ I2S";
@@ -19,7 +18,10 @@ void app_main(void)
 {
     init_i2s_audio();
     espnow_global_init();
-    test_send_sine_wave();
-    //espnow_create_send_i2s_task();
+
+    //test_send_sine_wave();
+
+    espnow_create_send_i2s_task();
+
     //test_i2s_audio();
 }
